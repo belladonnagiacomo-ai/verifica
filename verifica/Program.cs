b96f5ed29ba39e3cc58082ac6be7619d586345c1
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2026-02-25 12:18:17 +0100

    Aggiungere i file di progetto.

---
+++ verifica/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace verifica
+{
+    internal class Program
+    {
+        static void completa(string parola, string alfabeto)
+        {
+            for(int i = 0; i < parola.Length; i++)
+            {
+                Console.Write(parola[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            string parola = "ILMATNOHRBC";
+            string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            completa(parola, alfabeto);
+        }
+    }
+}
